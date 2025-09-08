859fc76b9af1f71a0567c5a1e0dd2f6e8bca4cb6
DESKTOP-633L9RU\Vitaly <vital.goloborodko@gmail.com>
2025-09-08 11:40:15 +0300

    make home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import { TextEffect } from '@/components/motion-primitives/text-effect';
 
 export default function Home() {
   return (
-    <div className="font-sans flex flex-col items-center justify-start min-h-screen p-8 pb-20  sm:p-20">
+    <div className="w-full flex flex-col items-center justify-start  mx-4 mx-auto font-sans text-center border-2 border-red-800">
       <h1>125</h1>
       <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
+        explicabo laborum repudiandae dolore eius? Dolores magni a fugit quod
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
         explicabo laborum repudiandae dolore eius? Dolores magni a fugit quod
         ex!
--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,5 +1,59 @@
 import React from 'react';
+import { Input } from './ui/input';
+import { Switch } from './ui/switch';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
-export const Calculator = () => {
-  return <div>Calculator</div>;
+export const Calculator = ({}) => {
+  // choice
+  return (
+    <div className="flex flex-col justify-center items-center border-2 border-blue-800 p-4 m-4">
+      <h1>Calculator</h1>
+      <label htmlFor="" className="flex  justify-center items-center">
+        <p>высота общая:</p>
+        <Input type="number" placeholder="1000" className="m-2" />
+        мм.
+      </label>
+      <label htmlFor="" className="flex  justify-center items-center">
+        <p>ширина общая:</p>
+        <Input type="number" placeholder="200" className="m-2" />
+        мм.
+      </label>
+      <label htmlFor="" className="flex  justify-center items-center">
+        <p>ширина рабочего проема:</p>
+        <Input type="number" placeholder="Type here..." className="m-2" />
+        мм.
+      </label>
+      <label htmlFor="" className="flex  justify-center items-center">
+        <p>ширина глухой части:</p>
+        <Input type="number" placeholder="Type here..." className="m-2" />
+        мм.
+      </label>
+      {/* <Input type="number" placeholder="Type here..." className="m-2" /> */}
+      {/* cabine */}
+      <hr />
+      {/* <label htmlFor="" className="flex  justify-center items-center">
+        <Switch />
+      </label> */}
+      <label htmlFor="" className="flex  justify-center items-center">
+        <p>Выбор типа стекла: </p>
+        <Select>
+          <SelectTrigger className="w-[300px]">
+            <SelectValue placeholder="тип стекла ..." />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="trasparent">прозрачное</SelectItem>
+            <SelectItem value="diamond">диаманд</SelectItem>
+            <SelectItem value="bronze">бронза</SelectItem>
+            <SelectItem value="graphite">графит</SelectItem>
+          </SelectContent>
+        </Select>
+      </label>
+    </div>
+  );
 };
